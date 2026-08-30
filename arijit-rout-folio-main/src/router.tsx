438cbf587313095355f1c7a8a9b7edd9b65f3de3
Arijit Rout <arijit28580@gmail.com>
2026-08-31 00:16:51 +0530

    fix

--- arijit-rout-folio-main/src/router.tsx
+++ arijit-rout-folio-main/src/router.tsx
@@ -7,7 +7,6 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
-    basepath: "/arijit-rout-folio/",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
